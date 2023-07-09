2d8c8e0cac360fb5de9df88dddb9d1ca77189a00
johnngure <johnwngure@gmail.com>
2023-07-09 13:50:46 +0300

    async

---
+++ JS ES6/asyn.js
@@ -0,0 +1,10 @@
+async function getProcessedData(url) {
+    let v;
+    try {
+      v = await downloadData(url);
+    } catch (e) {
+      v = await downloadFallbackData(url);
+    }
+    return processDataInWorker(v);
+  }
+  
